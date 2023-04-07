1392eddaa4c382579a89a2b557677ae16a1858fe
Daniel Quinn <dandidit.quinn@gmail.com>
2023-04-07 14:33:57 -0700

    2q

--- js/app.js
+++ js/app.js
@@ -53,11 +53,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let firstSum = sum (a, b)[0];
+  let finalSum = sum(firstSum, c)[0];
+  let firstP = multiply (a, b)[0];
+  let finalP = multiply (firstP, c)[0];
+  console.log (finalSum);
+  console.log (finalP);
+  return [finalSum, finalP, `${a} and ${b} and ${c} sum to ${finalSum}.`, `The product of ${a} and ${b} and ${c} is ${finalP}.`];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
